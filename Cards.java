ea1328638ce185a35588344223ecf08e0d8e6ac5
GodlyCarrot2 <rquan80@ladueschools.net>
2022-12-09 18:54:59 +0000

    12:55 12/9/2022

---
+++ Cards.java
@@ -0,0 +1,5 @@
+public class Cards {
+    private String cardName;
+    private int value;
+    
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main { 
     public static void main(String[] args) { 
         System.out.println("Crazy Eights!"); 
+
+        ArrayList<String> cards = new ArrayList<String>();
+        
+        int count = 0;
+        for (int i = 0; i<52; i++) {
+            cards.add("hi");
+        }
+        System.out.println(cards);
     }
 }
