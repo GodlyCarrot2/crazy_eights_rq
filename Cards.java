b1baadee8836322af4c947e7378bbb0157b3339f
GodlyCarrot2 <rquan80@ladueschools.net>
2022-12-13 18:51:21 +0000

    12/13/2022

--- Cards.java
+++ Cards.java
@@ -2,4 +2,29 @@ public class Cards {
     private String cardName;
     private int value;
     
+    public Cards() {
+        cardName = "";
+        value = 0;
+    }
+
+    public Cards(String a, int b) {
+        cardName = a;
+        value = b;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String a) {
+        cardName = a;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int b) {
+        value = b;
+    }
 }
--- Main.java
+++ Main.java
@@ -5,12 +5,39 @@ public class Main {
     public static void main(String[] args) { 
         System.out.println("Crazy Eights!"); 
 
-        ArrayList<String> cards = new ArrayList<String>();
+        ArrayList<Cards> cards = new ArrayList<Cards>();
         
         int count = 0;
-        for (int i = 0; i<52; i++) {
-            cards.add("hi");
+        /*
+        for (int i = 0; i<=52; i++) {
+            String x = Integer.toString(i);
+            Cards card = new Cards(x, 0);
+            cards.add(card);
         }
-        System.out.println(cards);
+        */
+
+        //hearts, spades, diamonds, clubs
+        for (int i = 0; i <=4; i++) {
+            String cardCat = "";
+            if (i==0) {
+                cardCat = "Hearts";
+            } else if (i==1) {
+                cardCat = "Spades";
+            } else if (i==2) {
+                cardCat = "Diamonds";
+            } else {
+                cardCat = "Clubs";
+            }
+
+            for (int i2 = 0; i2<=10; i2++) {
+                
+            }
+        }
+
+        for (int i = 0; i<=52; i++) {
+            System.out.print(cards.get(i).getCardName()+" ");
+            System.out.println(cards.get(i).getValue());
+        }
+        System.out.println(cards.get(0).getValue());
     }
 }
