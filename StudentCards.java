27713cc98130cb5a657d9a03df71b5a0399f7b53
GodlyCarrot2 <115576391+GodlyCarrot2@users.noreply.github.com>
2022-12-18 23:40:35 -0600

    Update StudentCards.java

--- StudentCards.java
+++ StudentCards.java
@@ -1,1 +1,44 @@
+public class Cards {
+    //creates the variables
+    private String cardName;
+    private int value;
+    private String cardType;
 
+    //makes the constructors
+    public Cards() {
+        cardName = "";
+        value = 0;
+        cardType = "";
+    }
+
+    //Creates the getters and setters
+    public Cards(String a, int b, String c) {
+        cardName = a;
+        value = b;
+        cardType = c;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String a) {
+        cardName = a;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int b) {
+        value = b;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String c) {
+        cardType = c;
+    }
+}
