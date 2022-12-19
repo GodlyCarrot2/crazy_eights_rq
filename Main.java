c961ab1fb636db1180962f30682215d87c7d5785
GodlyCarrot2 <rquan80@ladueschools.net>
2022-12-19 03:24:07 +0000

    aefaef

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main { 
     public static void main(String[] args) {  
@@ -67,7 +68,7 @@ public class Main {
         int cards = 52;
         System.out.println("Here is your hand, "+p1Name);
         for (int d=0; d<=4; d++ ) {
-            int rand = (int) ((Math.random()*cards));
+            int rand = (int) ((Math.random()*deck.size()));
             Cards y = new Cards(deck.get(rand).getCardName(), deck.get(rand).getValue(), deck.get(rand).getCardType());
             deck.remove(rand);
             hand1.add(y);
@@ -76,7 +77,7 @@ public class Main {
         }
 
         for (int d=0; d<=4; d++ ) {
-            int rand = (int) ((Math.random()*cards));
+            int rand = (int) ((Math.random()*deck.size()));
             Cards y = new Cards(deck.get(rand).getCardName(), deck.get(rand).getValue(), deck.get(rand).getCardType());
             deck.remove(rand);
             comphand.add(y);
@@ -84,7 +85,7 @@ public class Main {
         }
 
         int win = 0;
-        int rand = (int) (1+(Math.random()*cards));
+        int rand = (int) (1+(Math.random()*deck.size()));
         deck.remove(rand);
         Cards topcard = new Cards(deck.get(rand).getCardName(), deck.get(rand).getValue(), deck.get(rand).getCardType());
         cards = cards-1;
@@ -126,12 +127,59 @@ public class Main {
                         
                         if ((hand1.get(play-1).getCardType().equals(topcard.getCardType()))||(hand1.get(play-1).getValue() == (topcard.getValue()))) {
                             topcard = hand1.get(play-1);
+                            discard.add(hand1.get(play-1));
                             hand1.remove(play-1);
                             work = 1;
                             System.out.print("\033[H\033[2J");
                             System.out.println("You played: "+topcard.getCardName());
+
+                            //has the AI choose a random card it can play
+                            int done = 0;
+                            while (done == 0) {
+                                int legal2 = 0;
+                                for (int d = 0; d < comphand.size(); d++) {
+                                    if ((comphand.get(d).getCardType().equals(topcard.getCardType())) || (comphand.get(d).getValue() == (topcard.getValue()))) {
+                                        legal2 = legal2+1;
+                                    }
+                                }
+                                if (legal2 == 0) {
+                                    //draws cards for the AI
+                                    rand = (int) ((Math.random()*deck.size()));
+                                    Cards y = new Cards(deck.get(rand).getCardName(), deck.get(rand).getValue(), deck.get(rand).getCardType());
+                                    deck.remove(rand);
+                                    comphand.add(y);
+
+                                    //reshuffles the deck if there are 0 cards left
+                                    if (deck.size() == 0) {
+                                        for (int i = discard.size(); i >= 1; i--) {
+                                            Cards z = new Cards(discard.get(i).getCardName(), discard.get(i).getValue(), discard.get(i).getCardType());
+                                            deck.add(z);
+                                            discard.remove(i);
+                                        }
+                                        Collections.shuffle(deck);
+                                        Cards z = new Cards(topcard.getCardName(), topcard.getValue(), topcard.getCardType()); 
+                                        deck.remove(z);
+                                        discard.add(z);
+                                        System.out.println("Deck Reset");
+                                    }
+
+                                } else if (legal2 > 0) {
+                                    Collections.shuffle(comphand);
+                                    for (Cards i: comphand) {
+                                        if ((i.getCardType().equals(topcard.getCardType())) || (i.getValue() == (topcard.getValue()))) {
+                                            topcard = i;
+                                            discard.add(i);
+                                            comphand.remove(i);
+                                            break;
+                                        }
+                                   }
+                                   done = 1;
+                                }
+                            }
+                            System.out.println("The AI played a: "+topcard.getCardName());
+
                         } else {
-                            System.out.println("You can't play that card. Please choose a different card.");
+                            System.out.println("\nYou can't play that card. Please choose a different card.\n");
                         }
                         
                     }
@@ -140,16 +188,41 @@ public class Main {
             } else if (decision.equals("C")) {
                 System.out.print("\033[H\033[2J");
                 System.out.println("You choose to draw a card!");
-                rand = (int) ((Math.random()*cards));
+                rand = (int) ((Math.random()*deck.size()));
                 Cards y = new Cards(deck.get(rand).getCardName(), deck.get(rand).getValue(), deck.get(rand).getCardType());
                 deck.remove(rand);
                 hand1.add(y);
                 System.out.println("You drew a: "+y.getCardName());
 
+                //reshuffles deck if 0
+                if (deck.size() == 0) {
+                    for (int i = discard.size(); i >= 1; i--) {
+                        Cards z = new Cards(discard.get(i).getCardName(), discard.get(i).getValue(), discard.get(i).getCardType());
+                        deck.add(z);
+                        discard.remove(i);
+                    }
+                    Collections.shuffle(deck);
+                    Cards z = new Cards(topcard.getCardName(), topcard.getValue(), topcard.getCardType()); 
+                    deck.remove(z);
+                    discard.add(z);
+                    System.out.println("Deck Reset");
+                }
+
             } else {
                 System.out.print("\033[H\033[2J");
                 System.out.println("Please enter the option A, B or C with a capital letter.");
             }
+
+            if (hand1.size() == 0) {
+                win = 1; 
+            } else if (comphand.size() == 0) {
+                win = 2;
+            }
+        }
+        if (win == 1) {
+            System.out.println("You Won!!");
+        } else if (win == 2) {
+            System.out.println("You lost.");
         }
     }
 }
