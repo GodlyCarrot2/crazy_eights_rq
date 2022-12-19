17bfdcc2339a276aae45186eafd6e31f49eb8044
GodlyCarrot2 <rquan80@ladueschools.net>
2022-12-19 04:56:46 +0000

    10:56 Lol

--- Main.java
+++ Main.java
@@ -58,13 +58,14 @@ public class Main {
         System.out.println("Welcome to Crazy Eights!!!");
         System.out.println("For information on how to play Crazy Eights please review the README file.");
         Scanner sc =  new Scanner(System.in);
-
+        
+        //Asks the user for their name
         System.out.println("Player, what is your name?");
         String p1Name = sc.nextLine();
 
         System.out.print("\033[H\033[2J");
 
-        //deals the cards out to both players
+        //deals the cards out to both the user and the computer
         int cards = 52;
         System.out.println("Here is your hand, "+p1Name);
         for (int d=0; d<=4; d++ ) {
@@ -84,22 +85,29 @@ public class Main {
             cards = cards-1;
         }
 
+        //initializes the game phase and the variable
         int win = 0;
+        //chooses the first top card
         int rand = (int) (1+(Math.random()*deck.size()));
         deck.remove(rand);
         Cards topcard = new Cards(deck.get(rand).getCardName(), deck.get(rand).getValue(), deck.get(rand).getCardType());
         cards = cards-1;
+        //while loop to keep the game running
         while(win==0) {
+            //asks the user for their action
             System.out.println("\nOptions: \nA) Look at hand. \nB) Play a card. \nC) Draw a card.");
-            System.out.println("\nThe top cards is: "+topcard.getCardName());
+            System.out.println("\nThe top cards is: "+topcard.getCardName()); //prints the top cards
             String decision = sc.nextLine();
 
+            //Allows the user to view their hand
             if (decision.equals("A")) {
                 System.out.print("\033[H\033[2J");
                 System.out.println("You choose to look at your hand!");
                 for (int d = 0; d < hand1.size(); d++) {
                     System.out.println(hand1.get(d).getCardName());
                 }
+            
+            //Lets the user play a card
 
             } else if (decision.equals("B")) {
                 System.out.print("\033[H\033[2J");
@@ -113,8 +121,10 @@ public class Main {
                     }
                 }
 
+                //Asked the user to draw a card if they don't have any playable card.
                 if (legal == 0) {
                     System.out.println("Sorry you don't have any matching cards. You must draw a card.");
+                //Ask the user which carde they want to play.
                 } else if (legal > 0) {
                     int work = 0;
                     while (work == 0) {
@@ -125,10 +135,11 @@ public class Main {
                         System.out.println("Select the Number (not value) of the card you want to play.");
                         int play = Integer.parseInt(sc.nextLine());
                         
-                        if ((hand1.get(play-1).getCardType().equals(topcard.getCardType())) || (hand1.get(play-1).getValue() == (topcard.getValue())) || hand1.get(play-1).getValue() == 8) {
-                            topcard = hand1.get(play-1);
-                            discard.add(hand1.get(play-1));
+                        if ((hand1.get(play-1).getCardType().equals(topcard.getCardType())) || (hand1.get(play-1).getValue() == (topcard.getValue())) || hand1.get(play-1).getValue() == 8) { //checks if the card chosen is valid
+                            topcard = hand1.get(play-1); //changes the topcard
+                            discard.add(hand1.get(play-1)); //adds the card to the discard array list
 
+                            //if they play an 8, this asks for the suit they want to change it to.
                             if (hand1.get(play-1).getValue() == 8) {
                                 Scanner scan = new Scanner(System.in);
                                 System.out.println("Which suit would you like to chance it to? \nA) Hearts \nB) Spades \nC) Clubs \nD) Diamonds");
@@ -152,9 +163,11 @@ public class Main {
                                 }
                             }
                             
+                            //removes the card from player hand and exits the loop
                             hand1.remove(play-1);
                             work = 1;
 
+                            //Informs the user of what they played
                             System.out.print("\033[H\033[2J");
                             System.out.println("You played: "+topcard.getCardName());
                             
@@ -167,6 +180,7 @@ public class Main {
                             //has the AI choose a random card it can play
                             int done = 0;
                             while (done == 0) {
+                                //checks if the computer has any legal moves.
                                 int legal2 = 0;
                                 for (int d = 0; d < comphand.size(); d++) {
                                     if ((comphand.get(d).getCardType().equals(topcard.getCardType())) || (comphand.get(d).getValue() == (topcard.getValue())) || comphand.get(d).getValue() == 8) {
@@ -174,7 +188,7 @@ public class Main {
                                     }
                                 }
                                 if (legal2 == 0) {
-                                    //draws cards for the AI
+                                    //draws cards for the AI if they cannot play a card
                                     rand = (int) ((Math.random()*deck.size()));
                                     Cards y = new Cards(deck.get(rand).getCardName(), deck.get(rand).getValue(), deck.get(rand).getCardType());
                                     deck.remove(rand);
@@ -193,7 +207,7 @@ public class Main {
                                         discard.add(z);
                                         System.out.println("Deck Reset");
                                     }
-
+                                //if the computer has a card they can play it chooses the first one it can play
                                 } else if (legal2 > 0) {
                                     Collections.shuffle(comphand);
                                     for (Cards i: comphand) {
@@ -204,10 +218,10 @@ public class Main {
                                             break;
                                         }
                                    }
-                                   done = 1;
+                                   done = 1; //breaks out of the loop
                                 }
                             }
-                            System.out.println("The AI played a: "+topcard.getCardName());
+                            System.out.println("The AI played a: "+topcard.getCardName()); //informs the user of what the computer played
 
                             //checks if the AI won
                             if (comphand.size() == 0) {
@@ -216,13 +230,14 @@ public class Main {
                             }
 
                         } else {
-                            System.out.println("\nYou can't play that card. Please choose a different card.\n");
+                            System.out.println("\nYou can't play that card. Please choose a different card.\n"); //informs the user if they choose a card they cannot play
                         }
                         
                     }
                 }
-
-            } else if (decision.equals("C")) {
+            
+            //The code for when the user draws a card
+            } else if (decision.equals("C")) { 
                 System.out.print("\033[H\033[2J");
                 System.out.println("You choose to draw a card!");
                 rand = (int) ((Math.random()*deck.size()));
@@ -244,12 +259,14 @@ public class Main {
                     discard.add(z);
                     System.out.println("Deck Reset");
                 }
-
+            
+            //The code for when the user doesn't input an A, B, C or D
             } else {
                 System.out.print("\033[H\033[2J");
                 System.out.println("Please enter the option A, B or C with a capital letter.");
             }
 
+            //checks if the user or computer has won
             if (hand1.size() == 0) {
                 win = 1; 
             } else if (comphand.size() == 0) {
@@ -257,7 +274,7 @@ public class Main {
             }
         }
         
-        //checks the win condition
+        //Informs the user if they won or lost
         if (win == 1) {
             System.out.println("You Won!!");
         } else if (win == 2) {
